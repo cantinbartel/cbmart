458da0df6be24b120905082475a707da1ed9f32d
cantin bartel <cantinbartel.pro@gmail.com>
2022-09-04 19:40:23 +0200

    scroll to the top react router dom function

--- client/src/index.js
+++ client/src/index.js
@@ -5,25 +5,15 @@ import store from './store'
 import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import ScrollToTop from './components/ScrollToTop'
 
 const container = document.getElementById("root")
 const rootContainer = ReactDOM.createRoot(container)
 rootContainer.render(
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </Provider>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
